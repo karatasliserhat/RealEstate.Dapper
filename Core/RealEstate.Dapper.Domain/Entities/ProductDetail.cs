ccc33ef18af304fffb2948eff3131d4bdd94cde8
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-07-15 13:50:23 +0300

    ProductDetail Tablosundaki Build year kolonu type alanı int e çekildi test ok.

--- Core/RealEstate.Dapper.Domain/Entities/ProductDetail.cs
+++ Core/RealEstate.Dapper.Domain/Entities/ProductDetail.cs
@@ -7,7 +7,7 @@
         public int BedRoomCount { get; set; }
         public int RoomCount { get; set; }
         public int GarageSize { get; set; }
-        public char BuildYear { get; set; }
+        public int BuildYear { get; set; }
         public decimal Price { get; set; }
         public string Location { get; set; }
         public string VideoUrl { get; set; }
---
+++ Infrastructure/RealEstate.Dapper.Persistence/Migrations/20240715104904_mig9.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstate.Dapper.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BuildYear",
+                table: "ProductDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BuildYear",
+                table: "ProductDetails",
+                type: "nvarchar(1)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}

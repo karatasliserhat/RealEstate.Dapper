776220f4f0d40a73150e5f002a54197e4f68a39a
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-08-17 15:42:04 +0300

    Api katmanında progrom.cs tarafındaki servisler Extensions sınıfına taşındı test ok.

---
+++ Presentation/RealEstate.Dapper.WebAPI/Containers/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RealEstate.Dapper.Application.Interface;
+using RealEstate.Dapper.Application.Service;
+using RealEstate.Dapper.Persistence.Context;
+using RealEstate.Dapper.Persistence.Repositories;
+using RealEstate.Dapper.WebAPI.Tools;
+using System.Data;
+using System.Reflection;
+
+namespace RealEstate.Dapper.WebAPI.Containers
+{
+    public static class Extensions
+    {
+        public static void ContinerDependencies(this IServiceCollection Services, IConfiguration Configuration)
+        {
+
+            
+
+            var connection = Configuration.GetConnectionString("DapperConncetion");
+            Services.AddDbContext<DapperContext>(opt =>
+            {
+                opt.UseSqlServer(connection);
+            });
+            Services.AddScoped<IDbConnection>(sp =>
+            {
+                return new SqlConnection(connection);
+            });
+
+            Services.Configure<JwtTokenModel>(Configuration.GetSection("JwtTokenModel"));
+
+            Services.AddScoped<IJwtTokenModel>(conf =>
+            {
+                return conf.GetRequiredService<IOptions<JwtTokenModel>>().Value;
+            });
+
+
+
+            Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            Services.AddApplicationService();
+            Services.AddScoped<JwtTokenGenerator>();
+            Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            Services.AddScoped<IProductRepository, ProductRepository>();
+            Services.AddScoped<IAboutDetailRepository, AboutDetailRepository>();
+            Services.AddScoped<IAboutServiceRepository, AboutServiceRepository>();
+            Services.AddScoped<IStepsGridRepository, StepsGridRepository>();
+            Services.AddScoped<IPopulerLocationRepository, PopulerLocationRepository>();
+            Services.AddScoped<ITestimonialRepository, TestimonialRepository>();
+            Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            Services.AddScoped<IStatisticsRepository, StatisticsRepository>();
+            Services.AddScoped<IContactRepository, ContactRepository>();
+            Services.AddScoped<IToDoListRepository, ToDolistRepository>();
+            Services.AddScoped<IAccountRepository, AccountRepository>();
+            Services.AddScoped<IEstateAgentDashboardRepository, EstateAgentDashboardRepository>();
+            Services.AddScoped<IMessageRepository, MessageRepository>();
+            Services.AddScoped<IProductDetailRepository, ProductDetailRepository>();
+            Services.AddScoped<IProductImagesRepository, ProductImagesRepository>();
+            Services.AddScoped<IAppUserRepository, AppUserRepository>();
+            Services.AddScoped<IProperyAmenityRepository, ProperyAmenityRepository>();
+            Services.AddScoped<ISubFeatureRepository, SubFeatureRepository>();
+        }
+    }
+}
--- Presentation/RealEstate.Dapper.WebAPI/Program.cs
+++ Presentation/RealEstate.Dapper.WebAPI/Program.cs
@@ -1,21 +1,11 @@
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using RealEstate.Dapper.Application.Interface;
-using RealEstate.Dapper.Application.Service;
-using RealEstate.Dapper.Persistence.Context;
-using RealEstate.Dapper.Persistence.Repositories;
+using RealEstate.Dapper.WebAPI.Containers;
 using RealEstate.Dapper.WebAPI.Hubs;
-using RealEstate.Dapper.WebAPI.Tools;
-using System.Data;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 
 builder.Services.AddCors(opt =>
 {
@@ -25,52 +15,12 @@ builder.Services.AddCors(opt =>
     });
 });
 builder.Services.AddSignalR();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connection = builder.Configuration.GetConnectionString("DapperConncetion");
-builder.Services.AddDbContext<DapperContext>(opt =>
-{
-    opt.UseSqlServer(connection);
-});
-builder.Services.AddScoped<IDbConnection>(sp =>
-{
-    return new SqlConnection(connection);
-});
-
-builder.Services.Configure<JwtTokenModel>(builder.Configuration.GetSection("JwtTokenModel"));
-
-builder.Services.AddScoped<IJwtTokenModel>(conf =>
-{
-    return conf.GetRequiredService<IOptions<JwtTokenModel>>().Value;
-});
-
-
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-
-builder.Services.AddApplicationService();
-builder.Services.AddScoped<JwtTokenGenerator>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IAboutDetailRepository, AboutDetailRepository>();
-builder.Services.AddScoped<IAboutServiceRepository, AboutServiceRepository>();
-builder.Services.AddScoped<IStepsGridRepository, StepsGridRepository>();
-builder.Services.AddScoped<IPopulerLocationRepository, PopulerLocationRepository>();
-builder.Services.AddScoped<ITestimonialRepository, TestimonialRepository>();
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IStatisticsRepository, StatisticsRepository>();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
-builder.Services.AddScoped<IToDoListRepository, ToDolistRepository>();
-builder.Services.AddScoped<IAccountRepository, AccountRepository>();
-builder.Services.AddScoped<IEstateAgentDashboardRepository, EstateAgentDashboardRepository>();
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-builder.Services.AddScoped<IProductDetailRepository, ProductDetailRepository>();
-builder.Services.AddScoped<IProductImagesRepository, ProductImagesRepository>();
-builder.Services.AddScoped<IAppUserRepository, AppUserRepository>();
-builder.Services.AddScoped<IProperyAmenityRepository, ProperyAmenityRepository>();
-builder.Services.AddScoped<ISubFeatureRepository, SubFeatureRepository>();
+builder.Services.ContinerDependencies(builder.Configuration);
 
 var app = builder.Build();
 
